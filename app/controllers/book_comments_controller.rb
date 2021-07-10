42f0041ec76fb822f55e68fa7555770b1ffc0b00
EC2 Default User <ec2-user@ip-172-31-10-89.ec2.internal>
2021-07-10 11:06:50 +0000

    second commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,11 +3,10 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @book_comment = BookComment.new(book_comment_params)
+    @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
-    @book_comment.user_id = current_user.id
     @book_comment.save
-      
+    redirect_to book_path(@book), notice: "You have created book successfully."
     
   end
 
